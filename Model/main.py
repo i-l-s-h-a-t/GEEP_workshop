cccbad696ea2a2f325dc996b5339fd5d3110bcc2
Ilshat Saifulin <I.S.Saifullin@tudelft.nl>
2022-10-13 10:03:22 +0200

    added comments, commented first 100-days run, plotting moved to separate file

--- Model/main.py
+++ Model/main.py
@@ -5,18 +5,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 redirect_darts_output('out.log')
+
 m = Model()
 m.init()
+
+# equilibrate (run simulation for 0.1 days)
 m.run_python(0.1)
 m.export_pro_vtk()
 
-m.set_wells()
-m.run_python(100, restart_dt=m.params.first_ts)
-m.export_pro_vtk()
+#exit()
 
-for a in range(10):
-    m.run_python(100)
+m.set_wells()
+# m.run_python(100, restart_dt=m.params.first_ts)
+# m.export_pro_vtk()
+
+#  simulation
+n_steps = 10
+time_step = 365  # days
+
+for a in range(n_steps):
+    m.run_python(time_step)
     m.export_pro_vtk()
+
 m.print_timers()
 m.print_stat()
 
@@ -28,23 +38,3 @@ time_data.to_pickle("darts_time_data.pkl")
 writer = pd.ExcelWriter('time_data.xlsx')
 time_data.to_excel(writer, 'Sheet1')
 writer.save()
-
-# rate plotting
-from darts.tools.plot_darts import *
-
-ax1 = plot_total_prod_oil_rate_darts(time_data, style='-', color='b')
-ax1.set(xlabel="Days", ylabel="Produced gas rate, sm$^3$/day")
-
-ax3 = plot_total_prod_water_rate_darts(time_data, style='-', color='b')
-ax3.set(xlabel="Days", ylabel="Produced water rate, sm$^3$/day")
-
-ax2 = plot_total_inj_gas_rate_darts(time_data, style='-', color='b')
-ax2.set(xlabel="Days", ylabel="Injected gas rate, sm$^3$/day")
-
-ax4 = plot_total_inj_water_rate_darts(time_data, style='-', color='b')
-ax4.set(xlabel="Days", ylabel="Injected water rate, sm$^3$/day")
-
-# ax5 = plot_temp_darts("PROD005", time_data, style='-', color='b')
-# ax5.set(xlabel="Days", ylabel="Temperature, K")
-
-plt.show()
